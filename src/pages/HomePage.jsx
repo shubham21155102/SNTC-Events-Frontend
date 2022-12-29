82f56ef37919c616da983441238e199ca137ed76
Shubham Kumar <shubham.kumar.min21@itbhu.ac.in>
2022-12-30 02:41:00 +0530

    designing

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,42 @@
+import { Form,Input } from "antd";
+import {Link} from "react-router-dom"
 import React from 'react';
 import '../pages/styles/Homepage.css'
+import "../styles/LogInStyle.css";
 const HomePage = () => {
+  const onFinishHandler = (values) => {
+  console.log(values);
+  }
   return (
     <>
-    <p className='eventdashboard'>
+    
+  <div className='form-container'>
+
+<Form layout='vertical' onFinish={onFinishHandler} className="card p-4 login-form" >    
+<div className="login-form-container1"><h1>
+      Event Dashboard
+    </h1></div>
+    <div className="login-form-container2">
+    <p className='username'>
+    Username:
+    </p>
+<Form.Item  name="email" required >
+<Input type='text'placeholder='sntc@iitbhu' />
+</Form.Item>
+  <p className='credential'>
+Password:
+</p>
+<Form.Item  name="password" required>
+  <Input type='text' placeholder='SNTC IITBHU' />
+</Form.Item>
+<Link to="/register" className='ms-2'>Don't have an account?
+</Link>
+<button className='btn btn-primary' type='submit'>Log In</button>
+</div>
+
+</Form>
+</div>
+    {/* <p className='eventdashboard'>
       Event Dashboard
     </p>
     <div className='container1'>
@@ -17,8 +50,8 @@ Password:
 <input type={'password'} className='password' placeholder='SNTC IITBHU' />
 <button className='loginbutton'>
     Log In
-</button>
-    </div>
+</button> 
+    </div>*/}
     </>
   )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,86 @@
-import React from 'react';
-import {} from "antd";
-const Register = () => {
+import {React,useState} from 'react'
+// import {} from "react-bootstrap";
+import logo from '../components/images/logo.svg';
+import "../styles/RegisterStyles.css";
+function Register() {
+  const [user,setUser]=useState({
+    email:"",
+    password:"",
+    name:"",
+   });
+   let name, value;
+   const handleInputs=(e)=>{
+    // console.log(e);
+    name=e.target.name;
+    value=e.target.value;
+    setUser({...useState,[name]:value})
+   }
   return (
-    <div>
-     
+    <>
+         <section className="vh-100"> {/* style="background-color: #eee;" */}
+  <div className="container h-100">
+    <div className="row d-flex justify-content-center align-items-center h-100">
+      <div className="col-lg-12 col-xl-11">
+        <div className="card text-black" >  {/* style="border-radius: 25px;"" */}
+          <div className="card-body p-md-5">
+            <div className="row justify-content-center">
+            <h1 className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">CLUB REGISTRATION</h1>
+              <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+
+
+
+                <form className="mx-1 mx-md-4">
+
+                  <div className="d-flex flex-row align-items-center mb-4">
+                    <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div className="form-outline flex-fill mb-0">
+                    <label className="form-label" for="form3Example1c">Club Name</label>
+                      <input type="text" id="form3Example1c" className="form-control" name='name'placeholder='Club Name' value={user.name} onChange={handleInputs} />
+                     
+                    </div>
+                  </div>
+
+                  <div className="d-flex flex-row align-items-center mb-4">
+                    <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                    <div className="form-outline flex-fill mb-0">
+                     
+                      <label className="form-label" for="form3Example3c">Club Id</label>
+                       <input type="email" id="form3Example3c" className="form-control" name='email' placeholder='Club Id' value={user.email}  onChange={handleInputs}/>
+                    </div>
+                  </div>
+
+                  <div className="d-flex flex-row align-items-center mb-4">
+                    <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                    <div className="form-outline flex-fill mb-0">
+                     
+                      <label className="form-label" for="form3Example4c">Password</label>
+                       <input type="password" id="form3Example4c" className="form-control" name='password' placeholder='Password' value={user.password}  onChange={handleInputs}/>
+                    </div>
+                  </div>
+
+
+                
+
+                  <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                    <button type="button" className="btn btn-primary btn-lg">Register</button>
+                  </div>
+
+                </form>
+
+              </div>
+              <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+    <img src={logo} className="img-fluid" alt="Sample image2" />
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+</section>
+    </>
   )
 }
 
